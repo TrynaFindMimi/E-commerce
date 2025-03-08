a7ab0b14350978e998b08402210385b3bce8c8f7
Sebastian <sebasnocach@gmail.com>
2025-03-08 16:50:51 -0400

    registro_eliminado

--- registro.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("register-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita el envío del formulario (ajústalo según tu lógica)
-    window.location.href = "login.html"; // Redirige a login
-});
