ae2c5a233e64b247fb4643f82c41ceea538eccfd
Sebastian <sebasnocach@gmail.com>
2025-03-08 15:01:44 -0400

    commit login&register

---
+++ registro.js
@@ -0,0 +1,4 @@
+document.getElementById("register-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita el envío del formulario (ajústalo según tu lógica)
+    window.location.href = "login.html"; // Redirige a login
+});
